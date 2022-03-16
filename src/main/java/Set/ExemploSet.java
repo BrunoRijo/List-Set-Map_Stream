f238eb3a73b1191b070d9c75ed7394c4f49c9aa2
BrunoRijo <bruno_rijo@hotmail.com>
2022-03-16 19:44:51 -0300

    Conjuntos Set.

--- src/main/java/Set/ExemploSet.java
+++ src/main/java/Set/ExemploSet.java
@@ -29,6 +29,43 @@ public class ExemploSet {
         }
         System.out.println("Exiba a soma dos valores: "+ soma);
 
+        //O tamanho do conjunto pode ser pegado através do método size().
+        System.out.println("Exiba a média das notas" + soma/notas.size());
+
+        System.out.println("Remova a nota 0:");
+        notas.remove(0d);
+        System.out.println(notas);
+
+        System.out.println("Remova as notas menores que 7 e exiba na lista: ");
+        Iterator<Double> iterator1 = notas.iterator();
+        while (iterator1.hasNext()){
+            Double next = iterator1.next();
+            if (next < 7) iterator1.remove();
+        }
+        System.out.println(notas);
+
+        //Para salvar a ordem em que foi informado é necessário criar um LinkedHashSet
+        System.out.println("Exiba todas as notas na ordem em que foram informados: ");
+        Set<Double> notas2 = new LinkedHashSet<>();
+        notas2.add(7d);
+        notas2.add(8.5);
+        notas2.add(9.3);
+        notas2.add(5d);
+        notas2.add(7d);
+        notas2.add(0d);
+        notas2.add(3.6);
+        System.out.println(notas2 + "Conjuntos Set não guarda objetos que se repetem");
+
+        System.out.println("Exiba todas as notas em ordem crescente: ");
+        Set<Double> notas3 = new TreeSet<>(notas2);
+        System.out.println(notas3);
+
+        System.out.println("Apague todo o conjunto");
+        notas.clear();
+
+        System.out.println("Conferindo se o conjunto está vazio: "+ notas.isEmpty());
+        System.out.println("Conferindo se o conjunto 2 está vazio: "+ notas2.isEmpty());
+        System.out.println("Conferindo se o conjunto 3 está vazio: "+ notas3.isEmpty());
     }
 
 }
